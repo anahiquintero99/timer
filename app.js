69dc72287f2d5c704e0e77f4a5f39e99c3baa7a2
Anahi Quintero <anahiquintero@MacBook-Air-de-Anahi.local>
2021-01-03 19:33:11 -0600

    Implement timer funcionality

---
+++ app.js
@@ -0,0 +1,64 @@
+//DOM Elements
+const form = document.querySelector('form');
+const timer = document.querySelector('.timer');
+const hoursLabel = document.querySelector('.hours');
+const minutesLabel = document.querySelector('.minutes');
+const secondsLabel = document.querySelector('.seconds');
+
+//Enums
+const SECONDS_IN_AN_HOUR = 3600;
+const SECONDS_IN_AN_MINUTE = 60;
+const SECOND = 1000;
+
+function formatTwoDigits(number) {
+  return number < 10 ? `0${number}` : number;
+}
+
+/**
+ * Transform seconds to hours, minutes and seconds
+ * 
+ * @param {Number} seconds - The total of seconds.
+ */
+function formatSecond(totalSecond) {
+  const hours = parseInt(totalSecond / SECONDS_IN_AN_HOUR);
+  const totalSeconsMinusHours = totalSecond % SECONDS_IN_AN_HOUR;
+  const minutes = parseInt( totalSeconsMinusHours / SECONDS_IN_AN_MINUTE);
+  const seconds = parseInt( totalSeconsMinusHours % SECONDS_IN_AN_MINUTE);
+
+  hoursLabel.textContent = formatTwoDigits(hours);
+  minutesLabel.textContent = formatTwoDigits(minutes);
+  secondsLabel.textContent = formatTwoDigits(seconds);
+}
+
+/**
+ * Transform hours, minutes and seconds to seconds.
+ */
+function getSeconds () {
+  const hours = Number(form.hours.value);
+  const minutes = Number(form.minutes.value);
+  const seconds = Number(form.seconds.value);
+  
+  const hoursInSeconds = hours * SECONDS_IN_AN_HOUR;
+  const minutesInSeconds = minutes * SECONDS_IN_AN_MINUTE;
+  
+  return hoursInSeconds + minutesInSeconds + seconds;
+}
+
+/**
+ * Initialize timer with the value provided by user.
+ * 
+ * @param {SubmiEvent} event 
+ */
+function startTimer(event) {
+  event.preventDefault();
+  let totalSecond = getSeconds();
+  formatSecond(totalSecond);
+
+  const intervelId = setInterval(() => {
+    totalSecond--;
+    formatSecond(totalSecond);
+    if (!totalSecond) clearInterval(intervelId);
+  }, SECOND);
+}
+
+form.addEventListener('submit', startTimer);
